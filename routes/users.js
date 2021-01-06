b0c594f1e0b952c22132b5205d130a3ff1393a5b
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-06 10:12:24 +0530

    User Model and its validation done

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
+const User = require("../models/User");
 // @route POST api/users
 // @desc Register a student or teacher
 // @access Public
-router.post('/', (req, res) => {
-    res.send('Register a student or teacher');
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add Name").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    // res.send('Register a student or teacher');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Connect DataBase
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.send({ msg: "Welcome to the College Management Portal" })
