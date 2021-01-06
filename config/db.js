f54ee906acba75e0652d5e56d9dc9efac0ede74c
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-06 09:38:28 +0530

    connect mongodb to our app done

--- config/db.js
+++ config/db.js
@@ -1,20 +1,45 @@
+// const mongoose = require("mongoose");
+// const config = require("config");
+// const db = config.get("mongoURI");
+
+// const connectDB = () => {
+//   mongoose
+//     .connect(db, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("MongoDB Connected"))
+//     .catch(err => {
+//         console.error(err.message);
+//         process.exit(1);
+//     })
+// };
+
+// module.exports = connectDB;
+
+
+// second way of connecting by using async await
+
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => {
-        console.error(err.message);
-        process.exit(1);
-    })
+      // these above 3 are just to ignore the warnigns
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
